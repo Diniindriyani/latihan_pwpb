d041d9d1fbbbd372de0af69314ee481c074a6db3
Diniindriyani <idini0383@gmail.com>
2022-02-07 13:37:44 +0700

    initial commit

---
+++ application/controllers/Hello.php
@@ -0,0 +1,12 @@
+<?php
+class Hello extends CI_Controller{
+
+    function index(){
+        echo "hallo dini";
+    }
+
+    function show(){
+        echo "I Make The World Better Place";
+    }
+}
+?>
